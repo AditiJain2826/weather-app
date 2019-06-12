e592471687246e2b822efd4c7d674e1d8e7801f9
aditi <aditijain2826@gmail.com>
2019-06-12 23:06:29 +0530

    location

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,51 @@
+// const weatherForm = document.querySelector('form')
+// const searchTerm = document.querySelector('input')
+// const msg1 = document.querySelector('#msg-1');
+// const msg2 = document.querySelector('#msg-2');
+
+
+// msg1.textContent = ""
+
+// weatherForm.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     location = searchTerm.value
+//     msg1.textContent = 'loading';
+//     fetch('/weather?address=' + location).then((res) => {
+//         res.json().then((data) => {
+//             console.log(data);
+//             msg1.textContent=data.forecast
+//             msg2.textContent=data.location
+//         }, (err) => {
+//             msg1.textContent = err;
+//         })
+//     }, (error) => {
+//         msg1.textContent = error;
+//     })
+// })
+console.log("client side js loaded")
+
 const weatherForm = document.querySelector('form')
-const searchTerm = document.querySelector('input')
+const searchTerm = document.querySelector('#place')
 const msg1 = document.querySelector('#msg-1');
 const msg2 = document.querySelector('#msg-2');
 
 
 msg1.textContent = ""
-
 weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    location = searchTerm.value
-    msg1.textContent = 'loading';
-    fetch('/weather?address=' + location).then((res) => {
+    e.preventDefault();
+    // location = searchTerm.value
+    msg1.textContent = 'loading...';
+    fetch('/weather?address='+location).then((res) => {
         res.json().then((data) => {
             console.log(data);
             msg1.textContent=data.forecast
             msg2.textContent=data.location
         }, (err) => {
-            msg1.textContent = err;
+            console.log(err)
+            msg1.textContent = error;
         })
     }, (error) => {
+        console.log(error)
         msg1.textContent = error;
     })
 })
